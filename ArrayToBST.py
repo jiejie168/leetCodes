bc7d673dea5d1a3c4402c4fcc553844f779cdf20
jiejie168 <41291332+jiejie168@users.noreply.github.com>
2020-04-03 22:40:26 +0200

    update-0304

--- ArrayToBST.py
+++ ArrayToBST.py
@@ -15,4 +15,22 @@ One possible answer is: [0,-3,9,-10,null,5], which represents the following heig
  -10  5
 """
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+class Solution:
+    def sortedArrayToBST(self, nums) -> TreeNode:
+
+        def to_bst(nums,start,end):
+            if start>end:
+                return None
+            mid=(start+end)//2
+            node=TreeNode(nums[mid])
+            node.left=to_bst(nums,start,mid-1)
+            node.right=to_bst(nums,mid+1,end)
+            return node
+        return to_bst(nums,0,len(nums)-1)
 
